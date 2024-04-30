eb56f619f1619d5911a40ad358b970ff68f71bfe
Orwellian <orwellian225@gmail.com>
2024-04-30 16:03:06 +0200

    replace: random pruning with random searching

--- roundrobin.py
+++ roundrobin.py
@@ -3,6 +3,7 @@ from reconchess.bots.random_bot import RandomBot
 from reconchess.bots.trout_bot import TroutBot
 from baseline2 import RandomSensing
 from improved import EntropicSense
+from improved2 import NonPrunedEntropic
 
 import sys
 
@@ -13,17 +14,17 @@ else:
     num_rounds = int(input("Number of rounds"))
     game_length = float(input("Seconds per game"))
 
-players = [RandomSensing, EntropicSense, RandomBot, TroutBot]
+players = [RandomSensing, NonPrunedEntropic, RandomBot, TroutBot]
 
 game_rotations = [
-    [(0,1), (2,3)],
-    [(0,2), (1,3)],
-    [(0,3), (1,2)],
+    [(0, 1), (2, 3)],
+    [(0, 2), (1, 3)],
+    [(0, 3), (1, 2)],
 
     # Swap Colours
-    [(1,0), (3,2)],
-    [(2,0), (3,1)],
-    [(3,0), (2,1)],
+    [(1, 0), (3, 2)],
+    [(2, 0), (3, 1)],
+    [(3, 0), (2, 1)],
 ]
 
 game_results = {}
@@ -36,14 +37,14 @@ for i in range(num_rounds):
     game_idx = i % len(game_rotations)
     game_rotation = game_rotations[game_idx]
 
-    print(f"Game\tWhite\t\t\tBlack\t\tResult")
-    print("-------------------------------------------------------")
+    print(f"Game\t{'White': <16}\t{'Black': <16}\t{'Result': <8}\tReason")
+    print(f"{'-':-<90}")
     for game in game_rotation:
         subgame = game_rotation.index(game)
-        print(f"{i + 1}.{subgame + 1}\t{players[game[0]].__qualname__}\t\t{players[game[1]].__qualname__}\t", end="", flush=True)
+        print(f"{i + 1}.{subgame + 1}\t{players[game[0]].__qualname__: <16}\t{players[game[1]].__qualname__: <16}", end="", flush=True)
 
         try:
-            game_result, _, _ = rc.play_local_game(players[game[0]](), players[game[1]](), seconds_per_player=game_length)
+            game_result, win_reason, _ = rc.play_local_game(players[game[0]](), players[game[1]](), seconds_per_player=game_length)
             if game_result is None:
                 game_result_str = "Draw"
                 game_results[players[game[0]]]["draw"] += 1
@@ -57,15 +58,16 @@ for i in range(num_rounds):
                 game_results[players[game[0]]]["loss"] += 1
                 game_results[players[game[1]]]["win"] += 1
 
-            print(f"{game_result_str}")
+            print(f"\t{game_result_str: <8}\t{win_reason}")
         except:
             print("(Error - Failed)")
 
+    print(f"{'-':-<90}")
 
-    print("-------------------------------------------------------")
-
-print("Agent Results")
-print("Agent\t\tWins\tLosses\tDraws\tWin %")
-print("-------------------------------------------------------")
+print(f"{'-':-<64}")
+print(f"{'Agent Results': <20}|\tScore\tWins\tLosses\tDraws\tWin %")
+print(f"{'-':-<20}|{'-':-<43}")
 for agent in players:
-    print(f"{agent.__qualname__}\t{game_results[agent]['win']}\t{game_results[agent]['loss']}\t{game_results[agent]['draw']}\t{game_results[agent]['win'] / num_rounds * 100:.2f}")
+    score = game_results[agent]['win'] * 1 + game_results[agent]['draw'] * 0.5
+    print(f"{agent.__qualname__: <20}|\t{score}\t{game_results[agent]['win']}\t{game_results[agent]['loss']}\t{game_results[agent]['draw']}\t{game_results[agent]['win'] / num_rounds * 100:.2f}")
+print(f"{'-':-<64}")
